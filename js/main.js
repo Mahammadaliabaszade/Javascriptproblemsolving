10f532d4bbcc77d79f338dded8c6d9f6b842178f
Mahammadaliabaszade <68659317+Mahammadaliabaszade@users.noreply.github.com>
2020-10-28 01:08:08 +0400

    problemsolve

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//tek ededler cixaran program
+// for (let num = 0; num < 100; num++) {
+//     if (num % 2 == 0) {
+//         continue;
+//     }
+//     console.log(num);
+// }
+// cut ededler cixaran proqram
+// for (let num = 0; num < 100; num++) {
+//     if (num % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(num);
+// }
+// function exam(bal) {
+//     if (bal < 51) {
+//         console.log("Imtahandan kece bilmediniz.");
+//     } else if (bal > 50 && bal < 61) {
+//         console.log("siz imtahandan E qiymeti almisiniz");
+//     } else if (bal > 60 && bal < 71) {
+//         console.log("Siz imtahandan D qiymeti almisiniz");
+//     } else if (bal > 70 && bal < 81) {
+//         console.log("Siz imtahandan C qiymeti almisiniz");
+//     } else if (bal > 80 && bal < 91) {
+//         console.log("Siz imtahandan B qiymeti almisiniz");
+//     } else if (bal > 90 && bal <= 100) {
+//         console.log("Siz imtahandan A qiymeti almisiniz");
+//     }
+// }
+// exam(73);
+// exam(53);
+// exam(67);
+// exam(87);
